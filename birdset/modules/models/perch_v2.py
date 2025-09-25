44f94314b5bb0a14bb158375f1eede87e13918d7
Raphael Schwinger <raphaelschwinger@gmail.com>
2025-09-25 13:20:34 +0000

    hack together to run perch_v2 on gpu

--- birdset/modules/models/perch_v2.py
+++ birdset/modules/models/perch_v2.py
@@ -10,7 +10,9 @@ from torch import nn
 from birdset.configs import PretrainInfoConfig
 
 
-from birdset.utils.perch_v2_utils.perch_hoplite.zoo.model_configs import load_model_by_name
+from birdset.utils.perch_v2_utils.perch_hoplite.zoo.model_configs import (
+    load_model_by_name,
+)
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -18,7 +20,6 @@ import pandas as pd
 
 SAMPLE_RATE = 32000
 LENGTH_IN_SAMPLES = 160000
-
 
 
 class PerchV2Model(nn.Module):
@@ -76,7 +77,6 @@ class PerchV2Model(nn.Module):
             self.hf_path = None
             self.hf_name = None
 
-        
         self.classifier = nn.Sequential(
             nn.Linear(self.EMBEDDING_SIZE, 128),
             nn.ReLU(),
@@ -92,27 +92,36 @@ class PerchV2Model(nn.Module):
         """
         Load the model from TensorFlow Hub.
         """
-        cudnn_version = (torch.backends.cudnn.version() % 1000) // 100
-        if cudnn_version >= 3:
-            physical_devices = tf.config.list_physical_devices("GPU")
-            if len(physical_devices) > 0:
-                tf.config.experimental.set_memory_growth(physical_devices[0], True)
-                tf.config.optimizer.set_jit(True)
-                model_choice = 'perch_v2'
-        else:
-            import os
-            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-            model_choice = 'perch_v2_cpu'
-        
+        import os
+
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        model_choice = "perch_v2_cpu"
+        # cudnn_version = (torch.backends.cudnn.version() % 1000) // 100
+        # if cudnn_version >= 3:
+        #     physical_devices = tf.config.list_physical_devices("GPU")
+        #     if len(physical_devices) > 0:
+        #         # tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        #         import os
+        #         os.environ["CUDA_VISIBLE_DEVICES"]="0"
+        #         tf.config.experimental.set_visible_devices(
+        #         physical_devices[0], "GPU"
+        #     )
+        #         tf.config.optimizer.set_jit(True)
+        #         model_choice = 'perch_v2'
+        # else:
+        #     import os
+        #     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        #     model_choice = 'perch_v2_cpu'
+
         perch_v2 = load_model_by_name(model_choice)
 
         self.model = perch_v2.embed
-                
-        self.class_label_key = 'label'
+
+        self.class_label_key = "label"
 
         if self.restrict_logits:
             # Extract the 'ebird2021' column as a list
-            pretrain_classlabels = perch_v2.class_list['labels'].classes
+            pretrain_classlabels = perch_v2.class_list["labels"].classes
 
             # Load dataset information
             dataset_info = datasets.load_dataset_builder(
@@ -140,7 +149,6 @@ class PerchV2Model(nn.Module):
             ]
             if missing_labels:
                 logging.warning(f"Missing labels in pretrained model: {missing_labels}")
-    
 
     def forward(
         self, input_values: torch.Tensor, labels: Optional[torch.Tensor] = None
@@ -163,11 +171,10 @@ class PerchV2Model(nn.Module):
 
         # Move the tensor to the CPU and convert it to a NumPy array.
         input_values = input_values.cpu().numpy()
-        
+
         input_values = tf.convert_to_tensor(input_values, dtype=tf.float32)
-        
+
         results = self.model(input_values)
-        
 
         if self.train_classifier:
             embeddings = results.embeddings
@@ -180,6 +187,6 @@ class PerchV2Model(nn.Module):
         return output
 
 
-p = PerchV2Model(14795)
-samples = torch.zeros(160_000)
-logits = p(samples)
+# p = PerchV2Model(14795)
+# samples = torch.zeros(160_000)
+# logits = p(samples)
