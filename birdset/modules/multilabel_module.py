bfb92e66d30ae45f46cf70608a04eb510a1eb6dd
Raphael Schwinger <raphaelschwinger@gmail.com>
2025-10-01 09:00:59 +0000

    make pipeline faster by removing one hot encoding for train split

--- birdset/modules/multilabel_module.py
+++ birdset/modules/multilabel_module.py
@@ -69,74 +69,76 @@ class MultilabelModule(BaseModule):
     def test_step(self, batch, batch_idx):
         test_loss, preds, targets = self.model_step(batch, batch_idx)
 
-        # save targets and predictions for test_epoch_end
-        self.test_targets.append(targets.detach().cpu())
-        self.test_preds.append(preds.detach().cpu())
+        # # save targets and predictions for test_epoch_end
+        # self.test_targets.append(targets.detach().cpu())
+        # self.test_preds.append(preds.detach().cpu())
 
-        self.log(
-            f"test/{self.loss.__class__.__name__}",
-            test_loss,
-            on_step=False,
-            on_epoch=True,
-            prog_bar=True,
-        )
+        # self.log(
+        #     f"test/{self.loss.__class__.__name__}",
+        #     test_loss,
+        #     on_step=False,
+        #     on_epoch=True,
+        #     prog_bar=True,
+        # )
 
-        self.test_metric(preds, targets.int())
-        self.log(
-            f"test/{self.test_metric.__class__.__name__}",
-            self.test_metric,
-            **asdict(self.logging_params),
-        )
+        # self.test_metric(preds, targets.int())
+        # self.log(
+        #     f"test/{self.test_metric.__class__.__name__}",
+        #     self.test_metric,
+        #     **asdict(self.logging_params),
+        # )
 
-        self.test_add_metrics(preds, targets.int())
-        self.log_dict(self.test_add_metrics, **asdict(self.logging_params))
+        # self.test_add_metrics(preds, targets.int())
+        # self.log_dict(self.test_add_metrics, **asdict(self.logging_params))
 
         return {"loss": test_loss, "preds": preds, "targets": targets}
 
     def on_test_epoch_end(self):
-        test_targets = torch.cat(self.test_targets).int()
-        test_preds = torch.cat(self.test_preds)
-        # self.test_complete_metrics(test_preds, test_targets)
+        # test_targets = torch.cat(self.test_targets).int()
+        # test_preds = torch.cat(self.test_preds)
+        # # self.test_complete_metrics(test_preds, test_targets)
 
-        log_dict = {}
+        # log_dict = {}
 
-        # Rename cmap to cmap5!
-        for metric_name, metric in self.test_complete_metrics.named_children():
-            value = metric(test_preds, test_targets)
-            log_dict[f"test/{metric_name}"] = value
+        # # Rename cmap to cmap5!
+        # for metric_name, metric in self.test_complete_metrics.named_children():
+        #     value = metric(test_preds, test_targets)
+        #     log_dict[f"test/{metric_name}"] = value
 
-        self.log_dict(log_dict, **asdict(self.logging_params))
+        # self.log_dict(log_dict, **asdict(self.logging_params))
 
-        if self.prediction_table:
-            self._wandb_prediction_table(test_preds, test_targets)
+        # if self.prediction_table:
+        #     self._wandb_prediction_table(test_preds, test_targets)
+        return None
 
     def _wandb_prediction_table(self, preds, targets):
-        top5_values_preds, top5_indices_preds = preds.topk(dim=1, k=5, sorted=True)
+        # top5_values_preds, top5_indices_preds = preds.topk(dim=1, k=5, sorted=True)
 
-        top5_values_preds = top5_values_preds.cpu().numpy()
-        top5_indices_preds = top5_indices_preds.cpu().numpy()
+        # top5_values_preds = top5_values_preds.cpu().numpy()
+        # top5_indices_preds = top5_indices_preds.cpu().numpy()
 
-        indices_targets = (targets == 1).nonzero(as_tuple=False)
-        multilabel_list = [[] for _ in range(targets.size(0))]
-        for idx in indices_targets:
-            row, col = idx.tolist()
-            multilabel_list[row].append(col)
+        # indices_targets = (targets == 1).nonzero(as_tuple=False)
+        # multilabel_list = [[] for _ in range(targets.size(0))]
+        # for idx in indices_targets:
+        #     row, col = idx.tolist()
+        #     multilabel_list[row].append(col)
 
-        # Convert the indices to strings
-        top5_indices_preds_str = [
-            ", ".join(map(str, indices)) for indices in top5_indices_preds
-        ]
-        multilabel_list_str = [
-            ", ".join(map(str, indices)) for indices in multilabel_list
-        ]
+        # # Convert the indices to strings
+        # top5_indices_preds_str = [
+        #     ", ".join(map(str, indices)) for indices in top5_indices_preds
+        # ]
+        # multilabel_list_str = [
+        #     ", ".join(map(str, indices)) for indices in multilabel_list
+        # ]
 
-        # Prepare the data for the wandb table
-        columns = ["Predictions", "Targets"]
-        data = [
-            [pred_str, tgt_str]
-            for pred_str, tgt_str in zip(top5_indices_preds_str, multilabel_list_str)
-        ]
-        table = wandb.Table(data=data, columns=columns)
+        # # Prepare the data for the wandb table
+        # columns = ["Predictions", "Targets"]
+        # data = [
+        #     [pred_str, tgt_str]
+        #     for pred_str, tgt_str in zip(top5_indices_preds_str, multilabel_list_str)
+        # ]
+        # table = wandb.Table(data=data, columns=columns)
 
-        # Log the table to wandb
-        wandb.log({"Top 5 Predictions vs Targets": table})
+        # # Log the table to wandb
+        # wandb.log({"Top 5 Predictions vs Targets": table})
+        return None
