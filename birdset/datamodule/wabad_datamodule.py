ea7ea3b93ec94c7581fc08d57e73ca18d7d3636a
BenMcEwen1 <benmcewen@outlook.com>
2025-10-02 16:29:01 +0200

    Initial setup of WABAD dataset

---
+++ birdset/datamodule/wabad_datamodule.py
@@ -0,0 +1,33 @@
+from datasets import Audio
+
+from . import BaseDataModuleHF
+from birdset.configs import DatasetConfig, LoadersConfig
+
+
+class WABADDataModule(BaseDataModuleHF):
+    def __init__(
+        self,
+        dataset: DatasetConfig = DatasetConfig(),
+        loaders: LoadersConfig = LoadersConfig(),
+    ):
+        super().__init__(
+            dataset=dataset,
+            loaders=loaders,
+        )
+
+    @property
+    def num_classes(self):
+        return 50
+
+    def _preprocess_data(self, dataset):
+        dataset = dataset.cast_column(
+            column="audio",
+            feature=Audio(
+                sampling_rate=self.dataset_config.sample_rate,
+                mono=True,
+                decode=True,
+            ),
+        )
+        # dataset = dataset.rename_column("target", "labels")
+        dataset = dataset.select_columns(["audio", "labels"])
+        return dataset
